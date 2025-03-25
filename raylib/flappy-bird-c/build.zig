796a45052e8eeff8823cd69ff23b03c43670a3cc
jpenn <jpenno123@hotmail.com>
2025-03-25 23:03:07 +1100

    add copy data dir to bin on build

--- raylib/flappy-bird-c/build.zig
+++ raylib/flappy-bird-c/build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void {
     // This creates another `std.Build.Step.Compile`, but this one builds an executable
     // rather than a static library.
     const exe = b.addExecutable(.{
-        .name = "raylib_c",
+        .name = "flappy-bird",
         .root_module = exe_mod,
     });
 
@@ -44,6 +44,14 @@ pub fn build(b: *std.Build) void {
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
     b.installArtifact(exe);
+
+    const install = b.getInstallStep();
+    const install_data = b.addInstallDirectory(.{
+        .source_dir = b.path("data"),
+        .install_dir = .{ .prefix = {} },
+        .install_subdir = "bin/data",
+    });
+    install.dependOn(&install_data.step);
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
