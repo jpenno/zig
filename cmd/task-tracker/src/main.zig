d59558df11fa64a61a705f945ef36b17b94a49a2
jpenn <jpenno123@hotmail.com>
2025-03-31 20:06:23 +1100

    add start parsing args

--- cmd/task-tracker/src/main.zig
+++ cmd/task-tracker/src/main.zig
@@ -1,13 +1,35 @@
 const std = @import("std");
 
+const Commands = enum {
+    add,
+    delete,
+    invalid_command,
+};
+
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // Get allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // Parse args into string array (error union needs 'try')
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Get and print them!
+    std.debug.print("There are {d} args:\n", .{args.len});
 
-    try bw.flush(); // Don't forget to flush!
+    const command = std.meta.stringToEnum(Commands, args[1]) orelse .invalid_command;
+
+    switch (command) {
+        .add => {
+            if (args.len > 2) {
+                std.log.info("add task: {s}", .{args[2]});
+            }
+        },
+        .delete => {},
+        .invalid_command => {
+            std.log.err("inavlid command: {s}", .{args[1]});
+        },
+    }
 }
